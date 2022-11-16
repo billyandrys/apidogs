df7a5d7a86269c4e13bb6aeac429720fb1e40c66
billyandryus <billyandrys@gmail.com>
2022-11-16 00:12:35 -0500

    validate connect postgre

--- src/db.js
+++ src/db.js
@@ -5,8 +5,9 @@ const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+//postgresql://postgres:${DB_PASSWORD}@containers-us-west-102.railway.app:7334/railway
+//postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}
+const sequelize = new Sequelize(`postgresql://postgres:${DB_PASSWORD}@containers-us-west-102.railway.app:7334/railway`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
