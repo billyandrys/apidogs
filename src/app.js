a05141018032b10819f03b9e8db97b581808806e
billyandryus <billyandrys@gmail.com>
2022-11-28 15:54:04 -0500

    refactory  cors()

--- src/app.js
+++ src/app.js
@@ -7,14 +7,14 @@ const cors = require('cors')
 require('./db.js');
 
 const server = express();
-//server.use(cors({origin:['https://iridescent-youtiao-b25959.netlify.app']}))
+server.use(cors({origin:['https://iridescent-youtiao-b25959.netlify.app']}))
 server.name = 'API';
 //, 'http://localhost:3000/'
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-
+server.use(cors())
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://iridescent-youtiao-b25959.netlify.app');
   //res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
