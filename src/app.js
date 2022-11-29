f746217265c95c8dbb9df355a9082dcdb144c1f4
billyandryus <billyandrys@gmail.com>
2022-11-28 21:36:18 -0500

    refactory ERROR CORS_

--- src/app.js
+++ src/app.js
@@ -16,8 +16,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(cors())
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://iridescent-youtiao-b25959.netlify.app/');
-  
+  res.header('Access-Control-Allow-Origin', 'https://iridescent-youtiao-b25959.netlify.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
